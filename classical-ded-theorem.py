78d53f5f8e8c6d5128c58c7cea16dca03c498e25
Vitor Greati <vitorgreati@gmail.com>
2023-04-05 11:17:37 +0200

    Simplify code

--- classical-ded-theorem.py
+++ classical-ded-theorem.py
@@ -1,12 +1,15 @@
 from typing import List, Tuple, Set
 
-CONNECTIVES = ["imp"]
+IMP = "->"
 
 class Fm:
-    def __init__(self, head : str, comps: List = None):
+    """
+    Represents an implicative formula.
+    """
+    def __init__(self, comps: List = None, head : str = IMP):
         self.head = head
         self.comps = comps
-        if head in CONNECTIVES:
+        if head == IMP:
             self.is_prop = False
         else:
             self.is_prop = True
@@ -23,9 +26,16 @@ class DerivLine:
         self.reason = reason
 
 class Derivation:
+
     def __init__(self, stmt : Tuple[Set[Fm], Fm], lines: List[DerivLine]):
         self.stmt = stmt
         self.lines = lines
+
+    def _make_axiom_1(self, fm1, fm2):
+        return Fm([fm1, Fm([fm2, fm1])])
+
+    def _make_axiom_2(self, fm1, fm2, fm3):
+        return Fm([Fm([fm1, Fm([fm2, fm3])]), Fm([Fm([fm1, fm2]), Fm([fm1, fm3])])])
     
     def deduction_theorem_derivation(self, X : Fm):
         new_lines = list()
@@ -34,16 +44,16 @@ class Derivation:
         old = 0
         old_new_map = dict()
         for original_line in self.lines:
-            new_fmla = Fm("imp", [X, original_line.fmla])
-            if (original_line.reason == "S" and original_line.fmla in S) or original_line.reason == "A" or original_line.reason == "Theorem":
+            new_fmla = Fm([X, original_line.fmla])
+            if (original_line.reason == "S" and original_line.fmla in S) or original_line.reason in {"A1", "A2"} or original_line.reason == "Theorem":
                 new_lines.append(original_line)
-                new_lines.append(DerivLine(Fm("imp", [original_line.fmla, Fm("imp", [X, original_line.fmla])]), "A"))
+                new_lines.append(DerivLine(self._make_axiom_1(original_line.fmla, X), "A1"))
                 idx += 2
                 new_lines.append(DerivLine(new_fmla, "MP %d %d" % (idx-1,idx-2)))
                 old_new_map[old] = idx
                 idx += 1
             elif X == original_line.fmla:
-                new_lines.append(DerivLine(Fm("imp", [original_line.fmla, original_line.fmla]), "Theorem"))
+                new_lines.append(DerivLine(Fm([original_line.fmla, original_line.fmla]), "Theorem"))
                 old_new_map[old] = idx
                 idx += 1
             else:
@@ -51,23 +61,18 @@ class Derivation:
                 Zk = self.lines[int(fm1)].fmla
                 Zj = self.lines[int(fm2)].fmla
                 Zi = original_line.fmla
-                new_lines.append(DerivLine(Fm("imp", 
-                                    [
-                                        Fm("imp", [X, Fm("imp", [Zj, Zi])]), 
-                                        Fm("imp", [Fm("imp", [X, Zj]), Fm("imp", [X, Zi])])
-                                    ])
-                                           , "A"))
+                new_lines.append(DerivLine(self._make_axiom_2(X, Zj, Zi), "A2"))
                 idx += 1
-                new_lines.append(DerivLine(Fm("imp", [Fm("imp", [X, Zj]), Fm("imp", [X, Zi])]), "MP %d %d" % (idx-1, old_new_map[int(fm1)])))
+                new_lines.append(DerivLine(Fm([Fm([X, Zj]), Fm([X, Zi])]), "MP %d %d" % (idx-1, old_new_map[int(fm1)])))
                 idx += 1
                 new_lines.append(DerivLine(new_fmla, "MP %d %d" % (idx-1, old_new_map[int(fm2)])))
                 old_new_map[old] = idx
                 idx += 1
             old += 1
-        return Derivation(stmt=(S, Fm("imp", [X, self.stmt[1]])), lines=new_lines)
-
+        return Derivation(stmt=(S, Fm([X, self.stmt[1]])), lines=new_lines)
 
     def print(self, trans):
+        print("%s |-- %s" % ([f.print(trans={}) for f in self.stmt[0]], self.stmt[1].print(trans={})))
         idx = 0
         width_fmla = max(map(lambda l : len(l.fmla.print(trans)), self.lines))+1
         width_reason = max(map(lambda l : len(l.reason), self.lines))+1
@@ -78,8 +83,8 @@ class Derivation:
 P = Fm(head="P")
 Q = Fm(head="Q")
 R = Fm(head="R")
-QiR = Fm(head="imp", comps = [Q, R])
-PiQiR = Fm(head="imp", comps = [P, QiR])
+QiR = Fm([Q, R])
+PiQiR = Fm([P, QiR])
 
 initial = \
     Derivation(
@@ -92,11 +97,14 @@ initial = \
             DerivLine(R, "MP 2 3"),
         ]
     )
-print("From: %s |-- %s" % ([f.print(trans={}) for f in initial.stmt[0]], initial.stmt[1].print(trans={})))
-initial.print(trans={})
 
+initial.print(trans={})
+print()
 D1 = initial.deduction_theorem_derivation(P)
+D1.print(trans={})
+print()
 D2 = D1.deduction_theorem_derivation(Q)
+D2.print(trans={})
+print()
 D3 = D2.deduction_theorem_derivation(PiQiR)
-print("To: %s |-- %s" % ([f.print(trans={}) for f in D3.stmt[0]], D3.stmt[1].print(trans={})))
 D3.print(trans={})
